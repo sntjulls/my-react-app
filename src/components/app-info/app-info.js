f9f12baa136651f9d3b7230ce8b179b62c07da25
“Yulia <julie.kovalchuk@gmail.com>
2023-02-26 13:27:41 +0200

    Initial commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting for employees in the company N</h1>
+      <h2>Total number of employees: </h2>
+      <h2>The award will be given to: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input
+        type='text'
+        className="form-control"
+        placeholder="Find an employee"
+        />
+    )
+}
+
+export default SearchPanel;
